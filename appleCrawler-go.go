ecc593b035492d7d88bec5520cd948e99136018b
Grimmer Kang <grimmer0125@gmail.com>
2016-07-17 13:14:58 +0800

    almost finish getallmacinfo

--- appleCrawler-go.go
+++ appleCrawler-go.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"runtime"
 	"strconv"
 	"strings"
 	"time"
+
+	_ "github.com/lib/pq" //why _ ?
 )
 
 // for debugging, may influence cpu and have side effect
@@ -49,12 +54,87 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func launchCrawer() {
 	StartCrawer(func(macs []Mac) {
-		fmt.Println("get callback:", macs)
+		// fmt.Println("get callback:", macs)
 	})
+}
+
+// postgres:///grimmer
+
+type Serverslice struct {
+	ImageURL    string `json:"imageURL"`
+	SpecsURL    string `json:"specsURL"`
+	SpecsTitle  string `json:"specsTitle"`
+	SpecsDetail string `json:"specsDetail"`
+	Price       string `json:"price"`
+}
+
+func describe(i interface{}) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
+
+func getAllMacInfo() {
+	db, err := sql.Open("postgres", "user=grimmer dbname=grimmer sslmode=disable")
+	if err != nil {
+		log.Fatal(err) // or panic(err)
+		fmt.Println("connect fail")
+
+	}
+
+	// age := 21
+	// rows, err := db.Query("SELECT name FROM users WHERE age = $1", age)
+	rows, err := db.Query("SELECT product_info FROM special_product_table")
+
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println("no get rows")
+
+	} else {
+		fmt.Println("get rows")
+	}
+
+	for rows.Next() {
+
+		fmt.Println("print row")
+		var s []byte
+		if err := rows.Scan(&s); err != nil {
+			log.Fatal(err)
+			fmt.Println("scan error")
+		}
+
+		// or like https://github.com/astaxie/build-web-application-with-golang/blob/master/zh/07.2.md
+		// another struct, s3 {s2 []Serverslice }
+		var s2 []Serverslice
+
+		// var jsonBlob = []byte(`[
+		// 	{"imageURL": "Platypus", "specsURL": "monotremata",
+		// 	"specsTitle":"cc", "specsDetail": "Monotremata", "Price":"abc"}
+		// ]`)
+		err := json.Unmarshal(s, &s2)
+
+		if err != nil {
+			fmt.Println("json error:", err)
+		}
+
+		// fmt.Println("scan ok,s:", s)
+		fmt.Println("scan ok,s2:", s2)
+	}
+
+	fmt.Println("final")
+
+	//need close
+
+	db.Close()
 }
 
 func main() {
 	// fmt.Println("in main, its id:", GoID())
+
+	mac1 := Mac{"1", "2", "pp"}
+	fmt.Println("mac1:", mac1)
+
+	getAllMacInfo()
+
+	fmt.Println("afte sql")
 
 	launchCrawer()
 
